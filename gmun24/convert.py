32ac525013aade5d77090250b22919e44b507c6b
Somya <isomya13@gmail.com>
2024-02-09 22:29:26 +0530

    added teammates

--- gmun24/convert.py
+++ gmun24/convert.py
@@ -27,21 +27,30 @@ def generate_svg(name, com, template_path, output_path):
 def main():
     
 
-    csv_file = "input.csv"  # Replace with your actual CSV file path
-    template_path = "template.svg"  # Replace with your actual template SVG path
-    output_folder = "output"  # Replace with your desired output folder path
-
-    # Create the output folder if it doesn't exist
+    csv_file = "input.csv"  
+    template_path = "template.svg" 
+    output_folder = "output" 
+    count =0
+ 
     os.makedirs(output_folder, exist_ok=True)
 
     with open(csv_file, "r") as file:
         reader = csv.reader(file)
-        next(reader)  # Skip header row
+        next(reader) 
 
         for row in reader:
             Country,name,Teammate,com = row
             output_file = os.path.join(output_folder, f"{name}_{com}.svg")
             generate_svg(name, com, template_path, output_file)
+            count+=1
+            if Teammate and Teammate.strip().lower() !='none':
+                generate_svg(Teammate, com, template_path, output_file)
+                count+=1
+
+    print(count)
+
+
+
 
 if __name__ == "__main__":
     main()
